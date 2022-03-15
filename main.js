03c1426fb17740b00464954236c74c0e6d8bc917
prajak-star <80954908+prajak-star@users.noreply.github.com>
2022-03-15 16:48:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+nosex=" ";
+nosey=" ";
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent("canva");
+	instializeInSetup(mario);
+	vid=createCapture(VIDEO);
+	vid.size(800,400);
+	vid.parent("console");
+	pwapk=ml5.poseNet(vid);
+	pwapk.on("pose",getem);
+}
+
+function getem (results) {
+	if (results.length>0) {
+		console.log("after results there is a dot , today you look likr a beautiful pot ;)");
+		console.log(results);
+		nosex= results[0].pose.nose.x;
+		nosey= results[0].pose.nose.y;
+	}
+}
+
+function draw() {
+	game();
+}
+
+
+
+
+
+
